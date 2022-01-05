59d0e6a7757f8dc3b5599d2209cc3b0e25bdd4fd
Weston Odend'hal <wodend@tuta.io>
2022-01-04 18:47:44 -0800

    Fix knot test

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,15 @@
-// #[test]
-// fn test_knot() {
-//     let sample_dir= "tests/samples/knot";
-//     let width = 10;
-//     let height = 10;
-//     let output_path = "tests/output/knot.png";
-//     wfc::run(sample_dir, width, height, output_path);
-//     assert!(true);
-// }
+#[test]
+fn test_knot() {
+    let sample_dir= "tests/samples/knot";
+    let width = 10;
+    let height = 10;
+    let output_path = "tests/output/knot.png";
+    wfc::run(sample_dir, width, height, output_path);
+    assert!(true);
+}
 
 #[test]
-fn test_stair() {
+fn test_stairs() {
     let sample_dir= "tests/samples/stairs";
     let width = 10;
     let height = 10;
